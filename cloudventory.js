6c1b3d9f069f9829ca63e01c04b1ede6b6cdc413
rifkirahman1911-hub <rifkirahman1911@gmail.com>
2025-11-09 15:09:20 +0700

    Update cloudventory.js

--- cloudventory.js
+++ cloudventory.js
@@ -28,3 +28,21 @@ window.addEventListener("load", () => {
 // ===== TAHUN OTOMATIS =====
 document.getElementById("year").textContent = new Date().getFullYear();
 
+// ===== SCROLL REVEAL EFFECT =====
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('show');
+    } else {
+      reveals[i].classList.remove('show');
+    }
+  }
+});
+
+
